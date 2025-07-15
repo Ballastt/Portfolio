2803d059badce1952296bd841bf7abf55a26f4d6
Ballast <birgit.leitner@yahoo.de>
2025-07-15 16:46:20 +0200

    standalone setting added

--- src/app/pages/home/above-the-fold/above-the-fold.ts
+++ src/app/pages/home/above-the-fold/above-the-fold.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-above-the-fold',
+  standalone: true,
   imports: [],
   templateUrl: './above-the-fold.html',
   styleUrl: './above-the-fold.scss'
