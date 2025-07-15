a4fc199cb0b3001cbc34dc74facdf2ac17190359
Ballast <birgit.leitner@yahoo.de>
2025-07-15 16:45:44 +0200

    comps imported

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Header } from '../home/header/header';
+import { AboutComponent } from '../about/about';
+import { AboveTheFold } from '../home/above-the-fold/above-the-fold';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [Header, AboveTheFold, AboutComponent],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
