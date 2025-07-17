63939ed775126a8be504a485c0a9bceb4f6c93ce
Ballast <birgit.leitner@yahoo.de>
2025-07-17 16:14:15 +0200

    Fonts reviewed again, styling changes for my footer-section, hover effects, form, social-media etc!

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { Header } from '../../shared/header/header';
-import { AboutComponent } from '../about/about';
-import { AboveTheFold } from '../home/above-the-fold/above-the-fold';
-import { Footer } from "../../shared/footer/footer";
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Header, AboveTheFold, AboutComponent, Footer],
+  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
