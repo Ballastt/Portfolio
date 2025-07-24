f8e1155f832d2bc285acf776c51e6bd8327f525d
Ballast <birgit.leitner@yahoo.de>
2025-07-24 13:09:17 +0200

    folder structure updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './shared/header/header';
 import { Footer } from './shared/footer/footer';
-import { AboveTheFold } from "./pages/home/above-the-fold/above-the-fold";
+import { AboveTheFold } from "./pages/above-the-fold/above-the-fold";
 import { AboutComponent } from "./pages/about/about";
 import { Skills } from "./pages/skills/skills";
 import { CommonModule } from '@angular/common';
 import { ProjectsComponent } from "./pages/projects/projects";
-import { References } from "./references/references";
+import { References } from "./pages/references/references";
 
 
 @Component({
