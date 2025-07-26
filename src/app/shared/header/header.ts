32a4ddc0eed98176b4b36a0e755038bfe1e4db6f
Ballast <birgit.leitner@yahoo.de>
2025-07-26 14:41:46 +0200

    overlay functionality worked into header comp

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Output, Input, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+  @Input() isOverlay = false;
+  @Output() toggleBurger = new EventEmitter<void>();
+
   activeLang: string = 'de';
 
   setLang(lang: string) {
     this.activeLang = lang;
-  }
 
-  toggleBurger() {}
+  }
 }
