2f4a55e7db3fb5144ce6965161cf92639da4f010
Ballast <birgit.leitner@yahoo.de>
2025-07-26 14:40:37 +0200

    new comp for social-media part of footer

---
+++ src/app/shared/footer-social-media/footer-social-media.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-social-media',
+  imports: [],
+  templateUrl: './footer-social-media.html',
+  styleUrl: './footer-social-media.scss'
+})
+export class FooterSocialMedia {
+
+}
