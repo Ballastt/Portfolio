99182400bd80adf8f4fac1e06e4813d8a327981e
Ballast <birgit.leitner@yahoo.de>
2025-07-26 14:42:08 +0200

    overlay functionality worked into root app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,43 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './shared/header/header';
 import { Footer } from './shared/footer/footer';
-import { AboveTheFold } from "./pages/above-the-fold/above-the-fold";
-import { AboutComponent } from "./pages/about/about";
-import { Skills } from "./pages/skills/skills";
+import { AboveTheFold } from './pages/above-the-fold/above-the-fold';
+import { AboutComponent } from './pages/about/about';
+import { Skills } from './pages/skills/skills';
 import { CommonModule } from '@angular/common';
-import { ProjectsComponent } from "./pages/projects/projects";
-import { References } from "./pages/references/references";
-
+import { ProjectsComponent } from './pages/projects/projects';
+import { References } from './pages/references/references';
+import { MenuOverlay } from './menu-overlay/menu-overlay';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, AboveTheFold, AboutComponent, Skills, CommonModule, ProjectsComponent, References],
+  imports: [
+    Header,
+    Footer,
+    AboveTheFold,
+    AboutComponent,
+    Skills,
+    CommonModule,
+    ProjectsComponent,
+    References,
+    MenuOverlay,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('portfolio');
+
+  burgerMenuOpen = false;
+
+    openMenu() {
+    this.burgerMenuOpen = true;
+  }
+
+  closeMenu() {
+    this.burgerMenuOpen = false;
+  }
+
 }
+  
+
