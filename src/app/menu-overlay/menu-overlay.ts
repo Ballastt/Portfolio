6332e9ace28f50d98ed663429c9172332ced7a5b
Ballast <birgit.leitner@yahoo.de>
2025-07-28 13:04:16 +0200

    imports added and used, methods added

--- src/app/menu-overlay/menu-overlay.ts
+++ src/app/menu-overlay/menu-overlay.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FooterSocialMedia } from '../shared/footer-social-media/footer-social-media';
+import { Header } from '../shared/header/header';
 
 @Component({
   selector: 'app-menu-overlay',
   standalone: true,
-  imports: [],  
+  imports: [FooterSocialMedia, Header],
   templateUrl: './menu-overlay.html',
-  styleUrls: ['./menu-overlay.scss'] 
+  styleUrls: ['./menu-overlay.scss'],
 })
 export class MenuOverlay {
-  activeSection = '';
+  activeSection: string = '';
+  navigateTo: string = '';
+  @Input() isOverlay = false; // von außen gesetzt
+  @Output() close = new EventEmitter<void>(); // Schließen-Signal
 
+  onToggleBurger() {
+    this.close.emit(); // nach außen weitergeben
+  }
+
+  navigate(sectionId: string): void {
+    this.activeSection = sectionId;
+    const el = document.getElementById(sectionId.toLowerCase());
+
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth' });
+      this.close.emit();
+    }
+  }
 }
