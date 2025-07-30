83932c918db8d63b0df36925139ce3cb1b5989d1
Ballast <birgit.leitner@yahoo.de>
2025-07-30 14:29:19 +0200

    quotes imgs deleted, instead svg path including aria-label to change their color on hover

--- src/app/pages/references/references.ts
+++ src/app/pages/references/references.ts
@@ -1,33 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ScrollArrow } from "../../shared/scroll-arrow/scroll-arrow";
+import { ScrollArrow } from '../../shared/scroll-arrow/scroll-arrow';
 
 @Component({
   selector: 'app-references',
   imports: [CommonModule, ScrollArrow],
   templateUrl: './references.html',
-  styleUrl: './references.scss'
+  styleUrl: './references.scss',
 })
 export class References {
   references = [
     {
-      imgSrc: 'assets/imgs/quote.png',
-      imgAlt: 'Ein Bild von Anführungszeichen',
       name: 'V. Schuster',
       role: 'Team Kollege',
-      text: 'Birgit hat dem Team mit ihren Organisationskills und ihrem Fokus geholfen. Wir wären ohne Sie nie so weit gekommen'
+      text: 'Birgit hat dem Team mit ihren Organisationskills und ihrem Fokus geholfen. Wir wären ohne Sie nie so weit gekommen',
     },
     {
-      imgSrc: 'assets/imgs/quote.png',
       name: 'V. Schuster',
       role: 'Team Kollege',
-      text: 'Birgit hat dem Team mit ihren Organisationskills und ihrem Fokus geholfen. Wir wären ohne Sie nie so weit gekommen'
+      text: 'Birgit hat dem Team mit ihren Organisationskills und ihrem Fokus geholfen. Wir wären ohne Sie nie so weit gekommen',
     },
     {
-      imgSrc: 'assets/imgs/quote.png',
       name: 'V. Schuster',
       role: 'Team Kollege',
-      text: 'Birgit hat dem Team mit ihren Organisationskills und ihrem Fokus geholfen. Wir wären ohne Sie nie so weit gekommen'
-    }
+      text: 'Birgit hat dem Team mit ihren Organisationskills und ihrem Fokus geholfen. Wir wären ohne Sie nie so weit gekommen',
+    },
   ];
 }
