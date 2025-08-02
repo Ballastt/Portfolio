473af1433fabe96ece14ca60d1066c4d5643e305
Ballast <birgit.leitner@yahoo.de>
2025-08-02 11:17:21 +0200

    new home component that route to home

---
+++ src/app/home/home.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Header } from '../shared/header/header';
+import { MenuOverlay } from '../menu-overlay/menu-overlay';
+import { AboveTheFold } from '../pages/above-the-fold/above-the-fold';
+import { AboutComponent } from '../pages/about/about';
+import { Skills } from '../pages/skills/skills';
+import { References } from '../pages/references/references';
+import { ProjectsComponent } from '../pages/projects/projects';
+import { Footer } from '../shared/footer/footer';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    Header,
+    MenuOverlay,
+    AboveTheFold,
+    AboutComponent,
+    Skills,
+    References,
+    ProjectsComponent,
+    Footer,
+    CommonModule
+  ],
+  templateUrl: './home.html',
+  styleUrls: ['./home.scss'],
+})
+export class Home {
+  burgerMenuOpen = false;
+
+  toggleMenu() {
+    this.burgerMenuOpen = !this.burgerMenuOpen;
+    document.body.style.overflow = this.burgerMenuOpen ? 'hidden' : '';
+  }
+
+  closeMenu() {
+    this.burgerMenuOpen = false;
+    document.body.style.overflow = '';
+  }
+}
