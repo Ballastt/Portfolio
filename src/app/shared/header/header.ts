d188090832ecacdcbd24d490401760838ccb7a61
Ballast <birgit.leitner@yahoo.de>
2025-08-02 12:17:11 +0200

    new input showBackButton and method to return home when in legal

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,5 +1,6 @@
 import { Component, Output, Input, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +11,15 @@ import { CommonModule } from '@angular/common';
 })
 export class Header {
   @Input() isOverlay = false;
+  @Input() showBackButton = false;
   @Output() toggleBurger = new EventEmitter<void>();
 
   activeLang: string = 'de';
+
+  constructor(private router: Router) {}
+  goHome() {
+    this.router.navigate(['/de/home']);
+  }
 
   setLang(lang: string) {
     this.activeLang = lang;
