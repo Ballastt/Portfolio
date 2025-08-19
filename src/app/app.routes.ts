2781c3d81eb9cbc10065fca734d384c1a4f679fe
Ballast <birgit.leitner@yahoo.de>
2025-08-19 10:52:28 +0200

    imports added, routing paths set up to Home  and  Legal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,16 @@
 import { Routes } from '@angular/router';
+import { Legal } from './legal/legal';
+import { Home } from './home/home';
 
 export const routes: Routes = [
-  
+  {
+    path: ':lang',
+    children: [
+      { path: 'home', component: Home }, // Hauptseite mit Inhalt
+      { path: 'datenschutz', component: Legal }, // Datenschutzseite
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+    ],
+  },
+  { path: '', redirectTo: 'de/home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'de/home' },
 ];
