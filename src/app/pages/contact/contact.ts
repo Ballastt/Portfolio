bf797f283621e47f6c304ebb128ff126ee5b6b5d
Ballast <birgit.leitner@yahoo.de>
2025-09-11 13:30:21 +0200

    refactor: ReactiveFormsModule imported, Formgroup used to create instances for form and onsubmit method added

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, NgForm } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
@@ -12,15 +17,23 @@ export class ContactComponent {
   isPrivacyHovered = false;
   isPrivacyChecked = false;
 
-  contactData = {
-    name: '',
-    email: '',
-    message: '',
-  };
+  applyForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', [
+      Validators.required,
+      Validators.minLength(100),
+    ]),
+    privacy: new FormControl(false, [Validators.requiredTrue]),
+  });
 
-  onSubmit(ngForm: NgForm) {
-    if (ngForm.valid && ngForm.submitted) {
-      console.log(this.contactData);
+  onSubmit() {
+    if (this.applyForm.valid) {
+      console.log(this.applyForm.value);
+      // Here you can handle the form submission, e.g., send data to a server
+    } else {
+      this.applyForm.markAllAsTouched();
+      console.log('Form is invalid');
     }
   }
 }
