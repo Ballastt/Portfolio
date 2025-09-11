cab689034a08cdd78ff7521730638e7e991d623c
Ballast <birgit.leitner@yahoo.de>
2025-09-11 13:28:27 +0200

    fix: commonModule imported, new methods added for navigation and triggering the setPressed on the overlay h1

--- src/app/menu-overlay/menu-overlay.ts
+++ src/app/menu-overlay/menu-overlay.ts
@@ -1,31 +1,35 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FooterSocialMedia } from '../shared/footer-social-media/footer-social-media';
-
 
 @Component({
   selector: 'app-menu-overlay',
   standalone: true,
-  imports: [FooterSocialMedia],
+  imports: [FooterSocialMedia, CommonModule],
   templateUrl: './menu-overlay.html',
   styleUrls: ['./menu-overlay.scss'],
 })
 export class MenuOverlay {
+  // Track, welche Überschrift gerade gedrückt wird
+  pressedHeadings: Record<string, boolean> = {};
   activeSection: string = '';
   navigateTo: string = '';
-  @Input() isOverlay = true; 
-  @Output() close = new EventEmitter<void>(); 
+
+  @Input() isOverlay = true;
+  @Output() close = new EventEmitter<void>();
+  @Output() navigate = new EventEmitter<'about' | 'skills' | 'projects'>();
 
   onToggleBurger() {
-    this.close.emit(); 
+    this.close.emit();
   }
 
-  navigate(sectionId: string): void {
-    this.activeSection = sectionId;
-    const el = document.getElementById(sectionId.toLowerCase());
+  // Hilfsmethode, um gedrückt zu setzen
+  setPressed(id: string, value: boolean) {
+    this.pressedHeadings[id] = value;
+  }
 
-    if (el) {
-      el.scrollIntoView({ behavior: 'smooth' });
-      this.close.emit();
-    }
+  goTo(section: 'about' | 'skills' | 'projects') {
+    this.close.emit(); // Overlay schließen
+    this.navigate.emit(section); // Home sagt dann: scroll dahin
   }
 }
