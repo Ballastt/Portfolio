1433835c26407909467c93cd6985c6964e149a0c
Ballast <birgit.leitner@yahoo.de>
2025-09-11 13:27:04 +0200

    fix: not needed components out of the imports

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Header } from '../shared/header/header';
 import { MenuOverlay } from '../menu-overlay/menu-overlay';
 import { AboveTheFold } from '../pages/above-the-fold/above-the-fold';
 import { AboutComponent } from '../pages/about/about';
@@ -7,19 +6,17 @@ import { Skills } from '../pages/skills/skills';
 import { References } from '../pages/references/references';
 import { ProjectsComponent } from '../pages/projects/projects';
 import { CommonModule } from '@angular/common';
-import { Footer } from '../shared/footer/footer';
 
 @Component({
+  standalone: true,
   selector: 'app-home',
   imports: [
-    Header,
     MenuOverlay,
     AboveTheFold,
     AboutComponent,
     Skills,
     References,
     ProjectsComponent,
-    Footer,
     CommonModule,
   ],
   templateUrl: './home.html',
