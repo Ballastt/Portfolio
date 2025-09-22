1d489bda6375966c213c4e21c482a7d9e545507d
Ballast <birgit.leitner@yahoo.de>
2025-09-22 15:41:04 +0200

    appConfig added to bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { appConfig } from './app/app.config';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(App, {
-  providers: [provideRouter(routes)],
-});
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers ?? []), // alle Provider aus appConfig übernehmen
+    provideRouter(routes), // zusätzlich den Router
+  ],
+}).catch((err) => console.error(err));
