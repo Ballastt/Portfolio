7888b2c001fef5c3ee7ea35c439810907785ea51
Ballast <birgit.leitner@yahoo.de>
2025-10-07 12:15:49 +0200

    chore: comments deleted, @angular/locilaze reference was automatically added

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+/// <reference types="@angular/localize" />
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { appConfig } from './app/app.config';
@@ -7,7 +9,7 @@ import { routes } from './app/app.routes';
 bootstrapApplication(App, {
   ...appConfig,
   providers: [
-    ...(appConfig.providers ?? []), // alle Provider aus appConfig übernehmen
-    provideRouter(routes), // zusätzlich den Router
+    ...(appConfig.providers ?? []), 
+    provideRouter(routes), 
   ],
 }).catch((err) => console.error(err));
