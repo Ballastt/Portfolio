18cda256ff65a468e8f3f855470ec92157c669fb
Ballast <birgit.leitner@yahoo.de>
2025-10-08 16:20:55 +0200

    new component added

---
+++ src/app/pages/not-found/not-found.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [],
+  templateUrl: './not-found.html',
+  styleUrl: './not-found.scss'
+})
+export class NotFound {
+
+}
