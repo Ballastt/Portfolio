e79a9eb4f9a47ceb0d929e91dca58ce1f25f1437
Ballast <birgit.leitner@yahoo.de>
2025-10-08 16:19:37 +0200

    fix: routes added (including englisch versions and a path to a notFound page)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { Legal } from './legal/legal';
 import { Home } from './home/home';
+import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'home', component: Home },
   { path: 'datenschutz', component: Legal },
-  { path: 'impressum', component: Legal }, // For English version
+  { path: 'impressum', component: Legal }, 
+  { path: 'privacy', component: Legal },
+  { path: 'imprint', component: Legal },
+  { path: '404', component: NotFound },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
